eaff29e461aaef7bb2549804609c2f3b3c8cba10
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2022-01-02 09:54:23 -0700

    fixed config/models

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,1 +1,15 @@
-ENTER CODE HERE
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,6 @@
-ENTER CODE HERE
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
+
+module.exports = { User, Product, Category, Order };
