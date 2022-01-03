beb659549aa1169f8a1db3bedd074d9b213fa1a2
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2022-01-03 08:14:44 -0700

    added pages/refactor

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,16 @@
- ENTER CODE HERE
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from '../components/Cart';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
+
+export default Home;
