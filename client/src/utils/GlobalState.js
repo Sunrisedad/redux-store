8cc05ee7ca48752b9b87e27b248e26effd950afc
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2022-01-03 09:57:04 -0700

    fixed utils folder

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,1 +1,23 @@
-ENTER CODE HERE
+ import React, { createContext, useContext } from "react";
+ import { useProductReducer } from './reducers';
+ const StoreContext = createContext();
+ const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+   const [state, dispatch] = useProductReducer({
+       products: [],
+      cart: [],
+      cartOpen: false,
+        categories: [],
+        currentCategory: ''
+      });
+    
+    console.log(state);
+     return <Provider value={[state, dispatch]} {...props} />;
+};
+
+ const useStoreContext = () => {
+     return useContext(StoreContext);
+ };
+
+ export { StoreProvider, useStoreContext };
